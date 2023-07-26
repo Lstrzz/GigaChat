9573bd7b3cc1f8acf58eb4a3e9442e4c6e2012b8
А.Б <LOSTOR@1hokage.ru>
2023-07-26 17:22:24 +0700

    Темная/Светлая тем Верхний заголовок Кнопка свернуть Кнопка развернуть Кнопка закрыть

--- GigaChat/MainWindow.xaml.cs
+++ GigaChat/MainWindow.xaml.cs
@@ -23,6 +23,57 @@ namespace GigaChat
         public MainWindow()
         {
             InitializeComponent();
+
         }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) 
+                DragMove();
+        }
+
+        private void Them_Click(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri(@"Themes\DarkThemeDictionary.xaml", UriKind.Relative);
+            ResourceDictionary resourceDictionary = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+        }
+        private void Them2_Click(object sender, RoutedEventArgs e)
+        {
+            //Выбор светлой темы
+            var uri = new Uri(@"Themes\LightThemeDictionary.xaml", UriKind.Relative);
+            ResourceDictionary resourceDictionary = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+        }
+
+        private void ButtonMimimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void ButtonMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                MainBorder.CornerRadius = new CornerRadius(0);
+                TitleBorder.CornerRadius = new CornerRadius(0);
+                WindowState = WindowState.Maximized;
+            }
+
+            else
+            {
+                MainBorder.CornerRadius = new CornerRadius(20);
+                TitleBorder.CornerRadius = new CornerRadius(20, 20, 0, 0);
+                WindowState = WindowState.Normal;
+            }
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
     }
 }
